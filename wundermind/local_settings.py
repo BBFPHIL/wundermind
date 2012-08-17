036be6075300d31f1dbd9a18d58d77230036949d
Phillip Walker <pwalker@fordham.edu>
2012-08-16 22:47:26 -0400

    First push, all initial files

---
+++ wundermind/local_settings.py
@@ -0,0 +1,28 @@
+from os import path, pardir
+import sys
+# Django settings for amaterasu project.
+
+PROJECT_ROOT = path.dirname(path.abspath(__file__))
+
+sys.path.append(path.join(PROJECT_ROOT, 'apps'))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': path.join(PROJECT_ROOT, pardir, 'db/wundermind.db'), # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
